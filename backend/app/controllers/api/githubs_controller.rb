83c82f611efc15d09d0f2d302a1d6d7e8a96c7ae
fuwa <fuwafuwa41.glr@gmail.com>
2025-12-31 22:49:12 +0900

    contributionsメソッドをリファクタ

--- backend/app/controllers/api/githubs_controller.rb
+++ backend/app/controllers/api/githubs_controller.rb
@@ -1,21 +1,34 @@
 class Api::GithubsController < ApplicationController
   def contributions
+    data = fetch_contributions
+    update_user_last_contribution_date(data)
+
+    render json: data
+  end
+
+  private
+
+  def fetch_contributions
     user = current_user
     # 将来的に1年前とcreated_atを比較して新しい方をfromにするかも
     from = user.created_at.utc.iso8601
     to = Time.current.utc.iso8601
 
-    service = GithubService.new(user.name)
-    data = service.fetch_contributions(from: from, to: to)
+    GithubService
+      .new(user.name)
+      .fetch_contributions(from: from, to: to)
+  end
 
-    last_contribution = data.reverse.find { |d| d[:count].to_i > 0 }
-    if last_contribution
-      user.last_contribution_date = last_contribution[:date]
-    end
+  def update_user_last_contribution_date(data)
+    last_contribution = last_contribution_day(data)
 
-    user.last_grass_check_date = Time.current
-    user.save!
+    current_user.update!(
+      last_contribution_date: last_contribution&.[](:date),
+      last_grass_check_date: Time.current
+    )
+  end
 
-    render json: data
+  def last_contribution_day(data)
+    data.reverse.find { |d| d[:count].to_i > 0 }
   end
 end
