bee024ff1a51416ea08b32df214f3cba9da180f9
fuwa <fuwafuwa41.glr@gmail.com>
2026-03-01 22:59:56 +0900

    sidekiqの設定をした

---
+++ backend/config/initializers/sidekiq.rb
@@ -0,0 +1,11 @@
+require 'sidekiq-cron'
+
+Sidekiq.configure_server do |config|
+  schedule_file = "config/sidekiq.yml"
+
+  if File.exist?(schedule_file)
+    yaml = YAML.load_file(schedule_file)
+    schedule = yaml.deep_symbolize_keys[:schedule]
+    Sidekiq::Cron::Job.load_from_hash(schedule) if schedule.present?
+  end
+end
