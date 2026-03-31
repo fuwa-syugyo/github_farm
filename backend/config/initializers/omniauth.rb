800edaf46a8313e6a0f794ac01eb41dce53b70e3
あるぱか <vicugnapacos@arupakanoMacBook-Pro.local>
2026-03-31 15:57:50 +0900

    環境変数の指定を修正

--- backend/config/initializers/omniauth.rb
+++ backend/config/initializers/omniauth.rb
@@ -1,14 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  if Rails.env.development? || Rails.env.test?
-    provider :github,
-      ENV["GITHUB_CLIENT_ID"],
-      ENV["GITHUB_CLIENT_SECRET"]
-  else
-    provider :github,
-      # 本番用のIDとSECRETを用意したら書き換える
-      Rails.application.credentials.github[:client_id],
-      Rails.application.credentials.github[:client_secret]
-  end
+  provider :github,
+    ENV.fetch("GITHUB_CLIENT_ID"),
+    ENV.fetch("GITHUB_CLIENT_SECRET")
 end
 
 OmniAuth.config.allowed_request_methods = [ :get, :post ]
